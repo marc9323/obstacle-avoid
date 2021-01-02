c2e540a90352cc241eb528a862c4b5e258479daa
Marc D. Holman <marcholman9323@gmail.com>
2021-01-02 17:16:08 -0600

    Player class

--- core/src/com/obstacleavoid/screen/GameScreen.java
+++ core/src/com/obstacleavoid/screen/GameScreen.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.obstacleavoid.config.GameConfig;
+import com.obstacleavoid.entity.Player;
 import com.obstacleavoid.util.GdxUtils;
 import com.obstacleavoid.util.ViewportUtils;
 
@@ -20,23 +21,46 @@ public class GameScreen implements Screen {
     private Viewport viewport;
     private ShapeRenderer renderer;
 
+    private Player player;
 
     @Override
     public void show () {
         camera = new OrthographicCamera();
         viewport = new FitViewport(GameConfig.WORLD_WIDTH, GameConfig.WORLD_HEIGHT, camera);
         renderer = new ShapeRenderer();
+
+        // create player
+        player = new Player();
+
+        // calculate position
+        float startPlayerX = 0;
+        float startPlayerY = 0;
+
+        // position player
+        player.setPosition(startPlayerX, startPlayerY);
     }
 
     @Override
     public void render(float delta) {
         GdxUtils.clearScreen();
 
+        //drawDebug();
+        renderDebug();
+    }
+
+    private void renderDebug() {
+        // so renderer know about our camera configuration
+        renderer.setProjectionMatrix(camera.combined);
+        renderer.begin(ShapeRenderer.ShapeType.Line);
+
         drawDebug();
+
+        renderer.end();
+        ViewportUtils.drawGrid(viewport, renderer);
     }
 
     private void drawDebug() {
-        ViewportUtils.drawGrid(viewport, renderer);
+       // ViewportUtils.drawGrid(viewport, renderer);
     }
 
     @Override
